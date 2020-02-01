e4668c54d5968dcf07119bdf82e128758452d99c
djtfoo <djt.foo@gmail.com>
2020-02-02 02:53:37 +0800

    Sign up scene done

---
+++ Assets/Scripts/SceneScripts/SignUpManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignUpManager : MonoBehaviour
+{
+    [SerializeField]
+    private InputField nameInputField;
+    [SerializeField]
+    private GameObject nextSceneButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSceneButton.SetActive(false);
+    }
+
+    public void OnTyping()
+    {
+        nextSceneButton.SetActive(nameInputField.text.Length != 0);
+    }
+
+    public void SaveNameToPlayerData()
+    {
+
+    }
+}
