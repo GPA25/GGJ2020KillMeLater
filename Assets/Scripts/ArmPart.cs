a32dc6c8098b0d7e6bdceeeebfa4f9a7db1729a0
WongAshley <wong.keng.han.ashley@gmail.com>
2020-02-01 12:07:28 +0800

    Added Leg and Hamd

---
+++ Assets/Scripts/ArmPart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmPart : BasePart
+{
+
+    // Dmage the arm will do
+    int damage = 1;
+
+    // how long before the attack starts
+    float windUpTime = 1.0f;
+
+    // How fast the attack animation will be
+    float attackSpeed = 1.0f;
+
+    // How long to finish the attack
+    float recoveryTime = 1.0f;
+
+    // How Long before the next attack
+    float attackDelay = 1.0f;
+}
---
+++ Assets/Scripts/LegPart.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegPart : BasePart
+{
+    public float moveSped = 1.0f;
+}
