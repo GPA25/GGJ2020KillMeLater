16d275f88b78543b29c550d9d904a6ecf2ac64cc
djtfoo <djt.foo@gmail.com>
2020-02-01 17:28:05 +0800

    Parts Database prefab

---
+++ Assets/Scripts/Database/PartData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PartsListData
+{
+    public PartData[] data;
+}
+
+
+[System.Serializable]
+public class PartData
+{
+    // BasePart
+    public string name;
+    public string fileName; // relative directory in Resources/Textures
+    public int price;
+    public int rarity;  // enum
+    public string flavorText;
+    public int partType;    // enum
+    // Torso
+    public float moveSpeedMult;
+    public float damageMult;
+    public float attackSpeedMult;
+    public float torsoAttackDelay;
+    // Leg
+    public float moveSpeed;
+    // Arm
+    public int damage;
+    public float windupTime;
+    public float attackSpeed;
+    public float recoveryTime;
+    public float armAttackDelay;
+}
---
+++ Assets/Scripts/Database/PartsTable.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartsTable : MonoBehaviour
+{
+    public static PartsTable instance = null;
+    public static PartsTable Instance
+    {
+        get { return instance; }
+    }
+
+    [SerializeField]
+    private TextAsset partsDatabase;
+
+    private Dictionary<string, PartData> partsTable;    // parts, hashed by part name
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Debug.Log(string.Format("Instance of PartsTable already exists! Deleting PartsTable script in %s.", gameObject.name));
+            Destroy(this);
+        }
+        partsTable = new Dictionary<string, PartData>();
+
+        InitPartsList();
+    }
+
+    private void InitPartsList()
+    {
+        PartsListData partsListData = JsonUtility.FromJson<PartsListData>(partsDatabase.text);
+        foreach (PartData data in partsListData.data)
+        {
+            partsTable.Add(data.name, data);
+        }
+    }
+
+    public PartData GetPartData(string partName)
+    {
+        if (partsTable.ContainsKey(partName))
+        {
+            return partsTable[partName];
+        }
+        return null;
+    }
+
+    public List<PartData> GetPartsByType(int type)  // change to enum
+    {
+        List<PartData> partsList = new List<PartData>();
+        foreach (var key in partsTable.Keys)
+        {
+            PartData part = partsTable[key];
+            if (part.partType == 4) // any part type
+            {
+                partsList.Add(part);
+            }
+            else if (part.partType == type)
+            {
+                partsList.Add(part);
+            }
+        }
+
+        return partsList;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
