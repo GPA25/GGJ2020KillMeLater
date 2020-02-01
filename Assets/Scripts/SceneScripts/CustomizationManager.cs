a326d72b739b74c9ebc6a7c14b0b46626fd802a7
JT <howcomee@outlook.com>
2020-02-01 18:16:20 +0800

    Just to push

--- Assets/Scripts/SceneScripts/CustomizationManager.cs
+++ Assets/Scripts/SceneScripts/CustomizationManager.cs
@@ -7,7 +7,7 @@ public class CustomizationManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string[] limbNameList = {"Muscle", "Muscle", "stockin", "banana"};
+        string[] limbNameList = {"stump", "stump", "stockin", "banana"};
         BasePart.LIMB_TYPE[] limbTypeList = {BasePart.LIMB_TYPE.LIMB_ARM, BasePart.LIMB_TYPE.LIMB_ARM, BasePart.LIMB_TYPE.LIMB_LEG, BasePart.LIMB_TYPE.LIMB_LEG};
 
         Character.Create("fish", "TestTorso", limbNameList, limbTypeList);       
