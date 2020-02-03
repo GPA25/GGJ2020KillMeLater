875cb7c6f3b5c2327d29d2d3d3dad3f3d6d3af86
djtfoo <djt.foo@gmail.com>
2020-02-03 08:59:21 +0800

    UI fix for different resolutions

---
+++ Assets/Scripts/CurrencyDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrencyDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private Text currencyText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currencyText.text = PlayerData.Instance.currency.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
